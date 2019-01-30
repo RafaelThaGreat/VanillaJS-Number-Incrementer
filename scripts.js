e66036c627da60ebfc97ef93804e73df2d631e9c
Rafael Marrero <32765360+RafaelThaGreat@users.noreply.github.com>
2019-01-30 18:07:55 -0500

    Version 1.0
    I got the number increment feature working and I will continue to work on this project until version 3 when it's got every functionality I set out to create and looks beautiful.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function incrementValue()
+{
+    let value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+    
+}
