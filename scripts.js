30d2c65f06952190b2f628127ccd59984500a2ab
Rafael Marrero <32765360+RafaelThaGreat@users.noreply.github.com>
2019-01-31 16:53:53 -0500

    Version 3

--- scripts.js
+++ scripts.js
@@ -1,18 +1,25 @@
-<<<<<<< HEAD
-function incrementValue()
-{
-    let value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('number').value = value;
-    
-=======
-function incrementValue()
-{
-    let value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('number').value = value;
-    
->>>>>>> e66036c627da60ebfc97ef93804e73df2d631e9c
+// Made by Hansome Boi
+// Rafael Marrero 2019
+
+function incrementValue(){
+    let number = parseInt(document.getElementById('zeronumber').value, 10);
+    number = isNaN(number) ? 0 : number;
+    number++;
+    document.getElementById('zeronumber').value = number;
+        
+}
+
+function increaseFontSize(id, increaseFactor){
+    txt = document.getElementById(id);
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + increaseFactor) + 'px';
+}
+
+function randomize() {
+    document.getElementById('zeronumber').style.color = randomColors();
+}
+  
+function randomColors() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
